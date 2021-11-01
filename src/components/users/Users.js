416b576276967a7a2cbea8d36c1019ae962ba91b
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-02 00:36:51 +0100

    Spinner Added to user component

---
+++ src/components/users/Users.js
@@ -0,0 +1,24 @@
+import React from "react";
+import UserItem from "./UserItem";
+import PropTypes from "prop-types";
+import Spinner from "../layouts/Spinner";
+
+const Users = ({ loading, users }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className="grid-3">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+export default Users;
