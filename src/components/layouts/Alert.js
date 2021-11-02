827ffbe23c52058442b52be0feee5dd17bda3472
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-02 21:26:12 +0100

    Alert context added to Alert component

--- src/components/layouts/Alert.js
+++ src/components/layouts/Alert.js
@@ -1,16 +1,15 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
-const Alert = ({ alert }) => {
+const Alert = () => {
+  const alertContext = useContext(AlertContext);
+  const { alert } = alertContext;
   return (
-      alert !== null && (
+    alert !== null && (
       <div className={`alert alert-${alert.type}`}>
         <i className="fas fa-info-circle"></i> {alert.msg}
       </div>
     )
-   ); 
-};
-Alert.propTypes = {
-  alert: PropTypes.object.isRequired,
+  );
 };
 export default Alert;
