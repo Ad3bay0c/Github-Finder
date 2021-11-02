a72fd90ec7de414cc89a4b69c53d978f9e5bc252
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-02 22:15:09 +0100

    More info added to About Page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,7 @@ const About = () => {
       <h1>About this App</h1>
       <p>App to search Github Users</p> 
       <p>Version 1.0</p>
+      <p><strong>Developed by Clinton (G-Tech)</strong> </p>
     </div>
   )
 }
