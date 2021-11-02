bca2348344a631080d0aa55ae42902c2c75ba9b8
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-02 12:54:19 +0100

    Links Added to the Navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
-import PropTypes from 'prop-types'
-
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 // class Navbar extends Component {
 //   static defaultProps = {
 //     title: "Github Finder",
@@ -23,23 +23,31 @@ import PropTypes from 'prop-types'
 //   }
 // }
 
-const Navbar = ({icon, title}) => {
+const Navbar = ({ icon, title }) => {
   return (
-    <nav className="nav bg-success">
+    <nav className="navbar bg-success">
       <h1>
         <i className={icon} />
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
-}
+};
 
 Navbar.defaultProps = {
   title: "Github Finder",
-  icon: "fab fa-github"
-}
+  icon: "fab fa-github",
+};
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
-}
+};
 export default Navbar;
